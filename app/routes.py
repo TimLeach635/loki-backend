2bad538db46cad4604064b7dc6e91f8232b69cc0
Tim Leach <Tim.Leach@softwire.com>
2020-01-22 13:47:21 +0000

    Removed all templating, other small changes

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, jsonify, request
+from flask import jsonify, request
 from app import app, db
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index/')
 def index():
     # Query database
     scoreboard = db.session.execute('SELECT first_name, last_name FROM players LIMIT 5;')
@@ -14,8 +14,8 @@ def index():
     # Return JSON
     return jsonify({'scoreboard': list(scoreboard_string_list)})
 
-@app.route('/players', methods=['GET', 'POST'])
-@app.route('/players/<player_id>')
+@app.route('/players/', methods=['GET', 'POST'])
+@app.route('/players/<player_id>/')
 def players(player_id=None):
     if request.method == 'GET':
         if player_id:
@@ -25,8 +25,8 @@ def players(player_id=None):
             players = db.session.execute('SELECT player_id, first_name, last_name FROM players;')
             return jsonify({'players': list(map(lambda player: {'player_id': player[0], 'first_name': player[1], 'last_name': player[2]}, players))})
 
-@app.route('/games')
-@app.route('/games/<game_id>')
+@app.route('/games/')
+@app.route('/games/<game_id>/')
 def games(game_id=None):
     if request.method == 'GET':
         if game_id:
@@ -36,8 +36,8 @@ def games(game_id=None):
             games = db.session.execute('SELECT game_id, name FROM games;')
             return jsonify({'games': list(map(lambda game: {'game_id': game[0], 'name': game[1]}, games))})
 
-@app.route('/matches')
-@app.route('/matches/<match_id>')
+@app.route('/matches/')
+@app.route('/matches/<match_id>/')
 def matches(match_id=None):
     if request.method == 'GET':
         if match_id:
@@ -62,5 +62,5 @@ def matches(match_id=None):
                     games.name AS game_name
                 FROM
                     matches LEFT JOIN games ON matches.game_id = games.game_id;
-                ''', {'match_id': match_id}).first()
+                ''', {'match_id': match_id})
             return jsonify({'matches': list(map(lambda match: {'match_id': match[0], 'match_date': match[1], 'game_name': match[2]}, matches))})
